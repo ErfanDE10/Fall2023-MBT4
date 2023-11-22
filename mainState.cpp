b7f4abdb342796144c50d33f2ede1c88945179c5
ErfanDE10 <150175418+ErfanDE10@users.noreply.github.com>
2023-11-22 18:48:39 +0330

    Update mainState2.cpp

--- mainState.cpp
+++ mainState.cpp
@@ -1,79 +1,80 @@
 #include <iostream>
+
 using namespace std ;
-enum class PatientState {
+
+enum PatientState {
     Healthy,
     Fever,
     Cough,
     Weakness,
-    choice ,
     Recovered 
 };
 
 class PatientStateMachine {
-public:
-     
-    PatientStateMachine() : currentState(PatientState::Healthy) {}
 
-   
-               
-    void transitionTo(PatientState newState , choice) {
+public:
+    
+    PatientStateMachine() : currentState(Healthy) {}
+
+
+    void transitionTo(int newState ) {
         switch (newState) {
-            case PatientState::Healthy:
+            case Healthy:
                   cout << "The patient changed to a healthy state. " << endl;
                   break;
-            case PatientState::Fever:
+            case Fever:
                   cout << "The patient became febrile." << endl;
                   break;
-            case PatientState::Cough:
+            case Cough:
                   cout << "The patient changed to a coughing state." << endl;
                   break;
-            case PatientState::Weakness:
+            case Weakness:
                   cout << "The patient changed to a weak state." << endl;
                   break;
-            case PatientState::Recovered:
+            case Recovered:
                   cout << "The patient changed to an improved condition." << endl;
                   break;
         }
-        currentState = newState;
+          currentState =  newState ;
     }
 
     void performAction() {
         switch (currentState) {
-            case PatientState::Healthy:
+            case Healthy:
                    cout << "The patient is healthy and does not need any operation." << endl;
                    break;
-            case PatientState::Fever:
+            case Fever:
                    cout << "The patient has a fever. Treat fever." << endl;
                    break;
-            case PatientState::Cough:
+            case Cough:
                    cout << "The patient coughs. Treat cough." << endl;
                    break;
-            case PatientState::Weakness:
+            case Weakness:
                    cout << "The patient is weak. Treat weakness." << endl;
                    break;
-            case PatientState::Recovered:
+            case Recovered:
                    cout << "The patient has recovered and does not need any operation." << endl;
                    break;
         }
     }
 
 private:
-    PatientState currentState;
+    int currentState;
 };
 
 int main() {
+    int  choice ;
+    cout << "please enter your statuse < Healthy : 0 , Fever : 1 , Cough = 2 , Weakness = 3 , Recovered = 4 " << endl ;
+    PatientStateMachine patient;
+   
 
- 
-     PatientStateMachine patient;
-     patient.performAction(); 
-     int choice ; 
-            cout << "please enter your statuse < Healthy : 0 , Fever : 1 , Cough = 2 , Weakness = 3 , Recovered = 4 " << endl ;
-            cin >> choice ; 
-    
-     while ( choice < 5 ) {
-    patient.transitionTo(choice);  
-    patient.performAction();  
-           }
 
-    return 0;
+    while ( choice < 5 ) {
+        cin >> choice ; 
+        patient.transitionTo(choice);  
+        patient.performAction();
+    }
+
+
+return 0;
 }
